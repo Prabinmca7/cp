cab320277390945e0b1350b33d260cc865a5de21
Prabin.gd <prabin.gd@oracle.com>
2024-09-02 17:32:01 +0530

    test

--- customer/development/views/pages/ask.php
+++ customer/development/views/pages/ask.php
@@ -16,8 +16,7 @@
         <p>#rn:msg:NEED_A_QUICKER_RESPONSE_LBL# <a href="/app/social/ask#rn:session#">#rn:msg:ASK_OUR_COMMUNITY_LBL#</a></p>
     </div>
 </div>
-<h2>DEMO PRABIN</h2>
-
+sample
 <div class="rn_PageContent rn_AskQuestion rn_Container">
     <form id="rn_QuestionSubmit" method="post" action="/ci/ajaxRequest/sendForm">
         <div id="rn_ErrorLocation"></div>
